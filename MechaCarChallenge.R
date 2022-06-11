79ee3ec1be7e1730e4aa4afb1deb985be3e2bcf1
matt overstreet <street.matt.over@gmail.com>
2022-06-11 16:41:49 -0500

    r script added

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,10 +1,17 @@
+#Import library
 library(dplyr)
+
+#Import CVSs
 mechaCar <- read.csv("MechaCar_mpg.csv")
+
 susCoil <- read.csv("Suspension_Coil.csv")
+
 # multiple linear regression
 linRegModel <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar) #generate multiple linear regression model
 linRegModel
 summary(linRegModel)
+
+
 #Suspension Coil Summaries
 #total summary
 total_summary <- summarize(susCoil, Mean = mean(PSI), Median = median(PSI), SD = sd(PSI), Variation = var(PSI))
